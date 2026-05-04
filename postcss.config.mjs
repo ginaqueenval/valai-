fa620ffc2ed8f8db4ec7115f179ca9ed17dc2566
ginaqueenval <akhalvat560@gmail.com>
2026-05-04 04:15:19 +0330

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,15 +1,8 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/lib/**/*.{js,ts,jsx,tsx,mdx}"
-  ],
-  theme: {
-    extend: {}
+const config = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  plugins: []
 };
 
 export default config;
